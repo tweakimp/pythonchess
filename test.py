8eab80f9e09b97ebd08bce446cb00936405f18f4
FW <tweakimp@gmail.com>
2018-01-01 14:02:12 +0100

    test

--- test.py
+++ test.py
@@ -1,21 +1,35 @@
-class MyClass(object):
-    def __init__(self, number, color):
-        self.number = number
+class piece(object):
+    def __init__(self, value, color, position):
+        self.value = value
         self.color = color
+        self.position = position
 
 
-my_objects = []
+def deletePiece(square):
+    for i in range(len(piecelist) - 1):
+        if piecelist[i].position == square:
+            print(f"Delete obj on position {piecelist[i].position}.")
+            del piecelist[i]
 
-for i in range(32):
-    if i % 2 == 0:
-        my_objects.append(MyClass(i, "w"))
-    else:
-        my_objects.append(MyClass(i, "b"))
 
-# later
+piecelist = []  # dictionary mit mehreren variablen
 
-for obj in my_objects:
-    if obj.number % 3 == 0:
-        del obj
-    else:
-        print(f"{obj.number} {obj.color}")
+for i in range(1, 33):
+    if 1 <= i <= 8:
+        piecelist.append(piece("p", "w", f"{i} 2"))
+        if(i == 1):
+            n1 = piece("k", "w", "4 9")
+            piecelist.append(n1)
+    elif 9 <= i <= 16:
+        piecelist.append(piece("p", "b", f"{i-8} 7"))
+
+for obj in piecelist:
+    print(f"{obj.color} {obj.value} {obj.position}")
+
+print("===============")
+deletePiece("1 7")
+deletePiece("6 2")
+print("===============")
+
+for obj in piecelist:
+    print(f"{obj.color} {obj.value} {obj.position}")
