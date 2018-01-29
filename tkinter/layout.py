7493589c145206a9dfad3287fbabd91882d7910e
Tweakimp <tweakimp@gmail.com>
2018-01-29 01:31:46 +0100

    dae

--- tkinter/layout.py
+++ tkinter/layout.py
@@ -27,26 +27,43 @@ class Example():
 
         # fill canvasArea
 
+        def clickCanvas(event):
+            self.history.insert(tk.END, event.widget.name + "\n")
+
+        highlightlist = ["A1", "B4", "E7"]
+
+        def applyHighlightcolor(event, highlightlist=highlightlist):
+            highlightcolor = "#e036f1"
+            for canvas in canvaslist:
+                if canvas.name in highlightlist:
+                    canvas.configure(background=highlightcolor)
+
+        canvaslist = []
         for i in range(board.height):
             for j in range(board.width):
                 color = "#343434" if (i + j) % 2 == 0 else "#ababab"
+
                 self.x = tk.Canvas(self.canvasArea, width=40, height=40,
                                    background=color, highlightthickness=0)
-                self.x.create_text(32, 32, fill="white", font="FiraMono 8 bold ",
+                self.x.create_text(32, 32, fill="gray", font="FiraMono 8 bold",
                                    text=f"{board.files[i]}{board.ranks[j]}")
                 self.x.grid(row=board.height - i, column=j)
+                # self.x.bind("<Button-1>", clickCanvas)
+                self.x.bind("<Button-1>", applyHighlightcolor)
+                self.x.name = f"{board.files[i]}{board.ranks[j]}"
+                canvaslist.append(self.x)
 
         # fill rightArea
 
         self.history = tkst.ScrolledText(self.rightArea, height=15, width=30)
         self.history.grid()
-        self.history.insert(tk.END, "Test\n\n\n\ndsfsdfsd")
-        self.root.mainloop()
+        self.history.insert(tk.END, "Test\n\n\n\ndsfsdfsd\n")
 
-        # create a Text widget with a Scrollbar attached
-        self.txt = tk.scrolledtext(self.root, undo=True)
-        self.txt.insert(tk.END, "tetss")
-        self.txt.pack(expand=True, fill='both')
+        # # create a Text widget with a Scrollbar attached
+        # self.txt = tk.scrolledtext(self.root, undo=True)
+        # self.txt.insert(tk.END, "tetss")
+        # self.txt.pack(expand=True, fill='both')
+        self.root.mainloop()
 
 
 if __name__ == '__main__':
